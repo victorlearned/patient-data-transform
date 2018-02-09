7961d3d145a1f101b77044cc1aea25cdbb6306a5
Victor Learned <victorlearned@gmail.com>
2018-02-08 16:13:00 -0800

    removed all the commented pre-refactored code

--- controllers/patientXmlController.js
+++ controllers/patientXmlController.js
@@ -9,37 +9,3 @@ class PatientXmlController extends AppController {
 }
 
 module.exports = PatientXmlController;
-
-// 'user strict';
-
-// var Patient = require("../models/patient");
-// var resolvePath = require("../lib/resolvePath");
-// var configPairing = require('../configs/data');
-
-// function getPatient(req, res) {
-//     let val = '';
-//     let patient = new Patient();
-//     for(let prop in configPairing.configXmlPairing) {
-//         val = resolvePath(req.body, configPairing.configXmlPairing[prop], null);
-//         if(!val) {
-//             res.send({ error: "Resource " + prop + " for "+ configPairing.configXmlPairing[prop] + " not found!"});
-//         }
-
-//         if(patient.hasOwnProperty(prop)) {
-//             patient[prop] = val;
-//         }
-//     }
-
-//     //Check date format
-//     if(isNaN(Date.parse(patient.dateOfBirth))) {
-//         res.send({ error: "Date not valid value for patient XML"});
-//     }
-
-//     let dob = new Date(patient.dateOfBirth).toISOString();
-//     patient.dateOfBirth = dob;
-//     res.send(JSON.stringify(patient));
-// }
-
-// module.exports = {
-//     getPatient: getPatient
-// };
--- routes.js
+++ routes.js
@@ -13,17 +13,3 @@ router.post("/patient", patientJsonController.getPatient);
 router.post('/patient-xml', xmlparser({trim: false, explicitArray: false}), patientXmlController.getPatient);
 
 module.exports = router;
-
-
-// var express = require("express");
-// var xmlparser = require("express-xml-bodyparser");
-// var Patient = require("./models/patient");
-// var patientJsonController = require("./controllers/patientJsonController");
-// var patientXmlController = require("./controllers/patientXmlController");
-// var router = express.Router();
-
-// router.post("/patient", patientJsonController.getPatient);
-
-// router.post('/patient-xml', xmlparser({trim: false, explicitArray: false}), patientXmlController.getPatient);
-
-// module.exports = router;
